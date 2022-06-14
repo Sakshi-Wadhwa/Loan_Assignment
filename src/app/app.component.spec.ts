fde2af629608b7e3f4ae991c4511889ae31cfe52
Sakshi-Wadhwa <57635853+Sakshi-Wadhwa@users.noreply.github.com>
2022-06-14 10:30:27 +0530

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'LoanSite'`, () => {
+  it(`should have as title 'Loansite'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('LoanSite');
+    expect(app.title).toEqual('Loansite');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('LoanSite app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Loansite app is running!');
   });
 });
